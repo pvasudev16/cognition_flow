f61abee7f4f82377e4ec95a5b8c48806778913b0
Pranai Vasudev <pranai.vasudev@gmail.com>
2023-08-22 21:23:06 -0700

    Isolate persona and write welcome prompts

--- main.py
+++ main.py
@@ -87,31 +87,90 @@ def main():
 
     # PERSONA
     ## Define our reader's persona
-    template = (
-        """You are a kind and compassionate assistant. Your main role is
-        to help people read. You are having a conversation with a human.
-        Tell the human that when they are ready to start they can tell
-        you. If the human indicates that they are ready to start
-        output the phrase "Let's go" exactly. Continue the conversation
-        with the human until they tell you they are ready to go.
-        
-        {chat_history}
-        
-        Human: {human_input}
-        Chatbot:"""
+    persona = (
+        """
+        You are a kind and compassionate assistant. Your main
+        role is to help people read. You use shorter sentences.
+        You are having a conversation with a human.
+        """
     )
 
     # WELCOME
     ## Make a chat prompt for the reader to welcome the user to
-    ## CogniFlow
-    chat_prompt = PromptTemplate(
-        input_variables=["chat_history", "human_input"],
-        template=template
+    ## CogniFlow. Human input should just be empty. Here, human_input
+    ## will be a dummy input, since the memory has it as the input_key.
+    welcome_prompt = PromptTemplate(
+        input_variables=[
+            "persona",
+            "chat_history",
+            "human_input"
+        ],
+        template=(
+            """
+            {persona}
+
+            Here is the chat history so far:
+            
+            {chat_history}
+
+            Welcome the user to CogniFlow. Make sure to use the words
+            "CogniFlow" in your welcome exactly. Tell the user they
+            can ask you any questions. If they ask you what CogniFlow
+            is, tell them that you are a reading assistant. You are
+            starting this conversation.
+
+            {human_input}
+
+            Chatbot:  
+            """
+        )
     )
 
-    conversation_chain = LLMChain(
+    welcome_chain = LLMChain(
         llm=llm,
-        prompt=chat_prompt,
+        prompt=welcome_prompt,
+        verbose=False,
+        memory=memory
+    )
+
+    welcome = welcome_chain.predict(
+        persona=persona,
+        human_input=""
+    )
+
+    print(welcome)
+
+    # INTRODUCTORY CONVERSATION
+    ## This is the converation after the bot has welcomed the user
+    ## to CogniFlow. This conversation goes on until the user tells
+    ## CogniFlow that they are ready to go.
+
+    intro_conversation_prompt = PromptTemplate(
+        input_variables=[
+            "persona",
+            "chat_history",
+            "human_input"
+        ],
+        template= (
+            """
+            {persona}
+            
+            Tell the human that when they are ready to start they
+            can tell you. If the human indicates that they are ready
+            to start output the phrase "Let's go" exactly. Continue
+            the conversation with the human until they tell you they
+            are ready to go.
+            
+            {chat_history}
+            
+            Human: {human_input}
+            Chatbot:"""
+        )
+    )
+
+    intro_conversation_chain = LLMChain(
+        llm=llm,
+        prompt=intro_conversation_prompt,
         verbose=False,
         memory=memory
     )
@@ -119,7 +178,10 @@ def main():
     ready_to_go = False
     while not ready_to_go:
         user_input = input("Human: ")
-        bot_output =  conversation_chain.predict(human_input=user_input)
+        bot_output =  intro_conversation_chain.predict(
+            human_input=user_input,
+            persona=persona
+        )
         print("Chatbot: " + bot_output)
         if "Let's go" in bot_output:
             ready_to_go = True
@@ -139,21 +201,31 @@ def main():
     sentences = document.sentences
     number_of_sentences_in_document = len(sentences)
 
-    # DOCUMENT SUMMARIZATION
-    # We have a conversation chat bot prompt template for the
-    # conversation. Use a different prompt to do
-    # the summaries of NUM_SENTENCES. After displaying the summary
-    # and the actual text, the conversation bot will converse with
-    # the user.
+    # Store the NUM_SENTENCES we get sequentially
+    summaries = []
 
-    intro_prompt = PromptTemplate(
+    # Cursor to track what sentence we're at in the text. Start
+    # at the beginning
+    c = 0
+
+    # INSTRUCTIONS
+    ## Here CogniFlow tells the user how to use CogniFlow. CogniFlow
+    ## will tell the user what it'll do and how the user can interact
+    ## with it. It'll ask the user if they are ready.
+
+    # Here, human_input is a dummy variable.
+    instruction_prompt = PromptTemplate(
         input_variables=[
             "human_input",
             "number_of_sentences",
+            "persona",
             "chat_history"
         ],
         template=(
-            """Here is the chat history so far:
+            """
+            {persona}
+
+            Here is the chat history so far:
             
             {chat_history}
             
@@ -165,31 +237,112 @@ def main():
             or to stop. If the user indicates they want to
             keep going, output "Let's keep going" exactly. If the user
             indicates they want to stop, output "Let's stop" exactly".
+            You are starting this new part of the conversation, so the
+            human will respond to what you say. Ask the user if they
+            are ready.
             
-            {human_input}"""
+            {human_input}
+
+            Chatbot:
+            """
         )
     )
+    instruction_chain = LLMChain(
+        llm=llm,
+        prompt=instruction_prompt,
+        memory=memory,
+        verbose=False
+    )
+    instruction_output = instruction_chain.predict(
+        human_input="",
+        persona=persona,
+        number_of_sentences=NUM_SENTENCES,
+    )
+    print("Chatbot: " + instruction_output)
 
-    # Note that this prompt does not accept any human_input. We are
-    # just 
+    human_input = input("Human: ")
+    check_if_ready_prompt = PromptTemplate(
+        input_variables=[
+            "human_input",
+            "chat_history",
+            "persona"
+        ],
+        template=(
+            """
+            {persona}
+
+            Here is the chat history so far:
+            {chat_history}
+
+            If {human_input} indicates that they are ready, output
+            the number 1 exactly. If {human_input} indicates that the
+            human is not ready output "That's ok, let me know when
+            you're ready" exactly.
+            """
+        )
+    )
+    check_if_ready_chain = LLMChain(
+        llm=llm,
+        prompt=check_if_ready_prompt,
+        memory=memory,
+        verbose=False
+    )
+    check_if_ready = check_if_ready_chain.predict(
+        persona=persona,
+        human_input=human_input,
+    )
+    print("Debugging: " + check_if_ready)
+
+    ready_to_go = False
+
+    if "1" in check_if_ready:
+        ready_to_go = True
+    while not ready_to_go:
+        print("Chatbot: " + check_if_ready)
+        human_input = input("Human input: ")
+        check_if_ready = check_if_ready_chain.predict(
+            persona=persona,
+            human_input=human_input,
+        )
+        if "1" in check_if_ready:
+            ready_to_go = True
+
+
+    ready_to_go = False
+    # SUMMARIZATION (PREPARATION)
+    ## Prepare the prompts and chains we'll need to do
+    ## the summarization.
+
+    # human_input is a dummy input
     summary_prompt = PromptTemplate(
         input_variables=[
-            "human_input", # human_input is actually the text we
-                           # want to summarize
+            "human_input",
+            "text_to_summarize",
+            "persona",
             "chat_history",
         ],
         template=(
-            """You are a kind and compassionate assistant. Your main
-            role is to help people read. Here is the chat history
-            so far:
+            """
+            {persona}
+            Here is the chat history so far:
         
             {chat_history}
 
             Now please summarize the following text in 10 words:
+
+            {text_to_summarize}
         
             {human_input}
+
+            Chatbot:
             """
         )
+    )
+    summary_chain = LLMChain(
+        llm=llm,
+        prompt=summary_prompt,
+        memory=memory,
+        verbose=False
     )
 
     # Make a prompt that will be used to discuss each of the summaries.
@@ -197,13 +350,16 @@ def main():
     discussion_prompt = PromptTemplate(
         input_variables=[
             "human_input",
+            "persona",
             "most_recent_summary",
             "summaries",
             "chat_history"
         ],
         template=(
-            """You are a kind and compassionate assistant. Your main role
-            is to help people read. Here is the chat history so far:
+            """
+            {persona}
+            
+            Here is the chat history so far:
 
             {chat_history}
             
@@ -219,7 +375,7 @@ def main():
             example, if they ask you to define a word, define it for
             them. If the human indicates that they are ready to continue
             then output the phrase "Let's keep going" exactly. If the
-            human indicates they are ready to quite, output the phrase
+            human indicates they are ready to quit, output the phrase
             "Let's stop" exactly.
 
             Human: {human_input}
@@ -227,47 +383,6 @@ def main():
             """
         )
     )
-
-    # Make a prompt that will thank the user for using CogniFlow
-    end_prompt = PromptTemplate(
-        input_variables=[
-            "human_input",
-            "chat_history"
-        ],
-        template=(
-            """This is the chat history so far:
-            {chat_history}
-
-            Tell the user that they made it to the end of the text
-            and that they did good work. Thank the user and make sure
-            to use "CogniFlow" in your thanks.
-            
-            {human_input}"""
-        )
-    )
-
-    # Sequentially get the next NUM_SENTENCES.
-    summaries = []
-
-    # Cursor to track what sentence we're at in the text. Start
-    # at the beginning
-    c = 0
-
-    # Specify which chain we want to use
-    intro_chain = LLMChain(
-        llm=llm,
-        prompt=intro_prompt,
-        memory=memory,
-        verbose=False
-    )
-
-    summary_chain = LLMChain(
-        llm=llm,
-        prompt=summary_prompt,
-        memory=memory,
-        verbose=False
-    )
-
     discussion_chain = LLMChain(
         llm=llm,
         prompt=discussion_prompt,
@@ -275,19 +390,8 @@ def main():
         verbose=False
     )
 
-    end_chain = LLMChain(
-        llm=llm,
-        prompt=end_prompt,
-        memory=memory,
-        verbose=False
-    )
-
-    # Get the bot to tell the user how to use this tool.
-    instruction_output = intro_chain.predict(
-        human_input="",
-        number_of_sentences=NUM_SENTENCES,
-    )
-    print(instruction_output)
+    # SUMMARIZATION CONVERSATION LOOP
+    ## Now we'll do the NUM_SENTENCES by NUM_SENTENCES summarization.
 
     # Flag indicating whether the user is ready to keep going or not
     # By default, assume they'll do one interation.
@@ -295,8 +399,10 @@ def main():
 
     while(keep_going):
         next_sentences = get_next_sentences(document, c, NUM_SENTENCES)
-        summary = summary_chain.run(
-            {"human_input" : next_sentences}
+        summary = summary_chain.predict(
+            text_to_summarize=next_sentences,
+            persona=persona,
+            human_input=""
         )
         print(
             "The summary of sentences "
@@ -325,6 +431,7 @@ def main():
             user_input = input("Human: ")
             discussion_output = discussion_chain.predict(
                 human_input=user_input,
+                persona=persona,
                 most_recent_summary=summary,
                 summaries=" ".join(summaries),
             )
@@ -336,7 +443,35 @@ def main():
                 keep_going=False
 
         print("\n\n")
+
+    # ENDING
+    ## Make a prompt that will thank the user for using CogniFlow
     
+    # Human input will be a dummy prompt.
+    end_prompt = PromptTemplate(
+        input_variables=[
+            "human_input",
+            "chat_history"
+        ],
+        template=(
+            """This is the chat history so far:
+            {chat_history}
+
+            Tell the user that they made it to the end of the text
+            and that they did good work. Thank the user and make sure
+            to use "CogniFlow" in your thanks.
+            
+            {human_input}
+            
+            Chatbot:"""
+        )
+    )
+    end_chain = LLMChain(
+        llm=llm,
+        prompt=end_prompt,
+        memory=memory,
+        verbose=False
+    )
     end_output = end_chain.predict(human_input="")
     print(end_output)
 
