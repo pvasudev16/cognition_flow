3ac3899ecfadf1ce7aa48fd1bf0aa0f945f6b47c
Pranai Vasudev <pranai.vasudev@gmail.com>
2023-09-24 15:57:32 -0700

    Use functional components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
@@ -6,19 +5,21 @@ import axios from 'axios'
 const App = () => {
   const [userInput, setUserInput] = useState("")
   const [summary, setSummary] = useState("")
+
+  // For debugging
   console.log(summary)
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     // Read the form data
     const form = e.target;
     const formData = new FormData(form);
-
-    // Pass formData as a fetch body directly
-    // axios.post('http://127.0.0.1:5000/', formData).then(response => setSummary(response.data.summary))
     setUserInput("")
     const response = await axios.post('http://127.0.0.1:5000/', formData)
     setSummary(response.data.summary)
+    // Could also have done
+    // axios.post('http://127.0.0.1:5000/', formData).then(response => setSummary(response.data.summary))
   }
 
   return (<div className="App">
@@ -51,78 +52,3 @@ const App = () => {
 }
 
 export default App
-// class Form extends React.Component {
-//   state = {
-//     summary: '',
-//   };
-
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     // Read the form data
-//     const form = e.target;
-//     const formData = new FormData(form);
-
-//     // Pass formData as a fetch body directly
-//     axios.post('http://127.0.0.1:5000/', formData).then(function (response) {
-//               console.log(response)
-//               return response.data;
-//           })
-//           .then(data =>
-//             this.setState({
-//               summary: data
-//             }));
-//           }
-  //   fetch('http://127.0.0.1:5000/', { // fetch("http://127.0.0.1:5000/test")
-  //     headers: {
-  //       Accept: 'application/json',
-  //     }, method: form.method, body: formData
-  //   })
-  //     .then(function (response) {
-  //       return response.json();
-  //     })
-  //     .then(data =>
-  //       this.setState({
-  //         summary: data
-  //       }));
-  // };
-
-  // Use an array-map a method in every array in javascript
-  // iterates thru the array. use a map method on every element.
-  // 
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <form onSubmit={this.handleSubmit}>{ // method="post"
-//             <label>
-//               Input the number of sentences you wish to summarize at
-//               a time
-//               <input name="numSentences" defaultValue=""/>
-//             </label>
-//           }
-//           <br></br>
-//             <label>
-//               Enter the text you want to summarize
-//               <textarea
-//                 name="postContent"
-//                 defaultValue=""
-//                 rows={20}
-//                 cols={80}
-//               />
-//             </label>
-//             <hr />
-//             <button type="submit">Get Summary</button>
-//             {/* <button type="submit">Get Summary</button> */}
-//           </form>
-//           <p>Your summary is: {this.state.summary['summary']}</p>
-//           <p>Cognition Flow</p>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
-
-// export default Form;
