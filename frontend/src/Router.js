06d39b46f20cb9a723eea1b5c084295e1e37521f
Pranai Vasudev <pranai.vasudev@gmail.com>
2023-11-20 19:30:59 -0800

    Add Register page and browser route to register page

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
-// import RegisterPage from "./pages/RegisterPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const Router = () => {
   return (
@@ -10,7 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" exact element={<LandingPage/>} />
         <Route path="/login" exact element={<LoginPage/>} />
-        {/* <Route path="/register" exact component={RegisterPage} /> */}
+        <Route path="/register" exact element={<RegisterPage />} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import httpClient from "../httpClient";
+
+const RegisterPage =() => {
+  const [email, setEmail] = useState("pvasudev@pvasudev.ca");
+  const [password, setPassword] = useState("Shrinkulitos");
+
+  const registerUser = async () => {
+    try {
+      await httpClient.post("//localhost:5000/register", {
+        email,
+        password,
+      });
+
+      window.location.href = "/";
+    } catch (error) {
+      if (error.response.status === 401) {
+        alert("Invalid credentials");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Create an account</h1>
+      <form>
+        <div>
+          <label>Email: </label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="sfsdf"
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="sdfsdf"
+          />
+        </div>
+        <button type="button" onClick={() => registerUser()}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
