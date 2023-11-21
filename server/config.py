12f41453145aa6b2684bfd6daf74f6c42d64fd8e
Pranai Vasudev <pranai.vasudev@gmail.com>
2023-11-20 19:34:12 -0800

    Remove redis from imports, update secret key, and add commented out SESSION_ variables to config.py

--- server/config.py
+++ server/config.py
@@ -1,6 +1,5 @@
 import dotenv
 import os
-import redis
 from models import db
 from datetime import timedelta
 
@@ -9,7 +8,7 @@ environment_values = dotenv.dotenv_values()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class ApplicationConfig:
-    SECRET_KEY = "aldkfjalsdjflaskdjflasjfdldas"
+    SECRET_KEY = "a very secret key"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
@@ -24,3 +23,7 @@ class ApplicationConfig:
     SESSION_SQLALCHEMY_TABLE="sessions"
     SESSION_SQLALCHEMY=db
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=15)
+
+    # SESSION_COOKIE_SECURE = "True" # or True?
+    # SESSION_COOKIE_SAMESITE = "None" # or None?
+    # SESSION_COOKIE_HTTPONLY = "False" # or False? Do I even need this
