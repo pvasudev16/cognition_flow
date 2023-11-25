ada068051f178ae563355f1a58aefccb2d5a53eb
Pranai Vasudev <pranai.vasudev@gmail.com>
2023-11-25 12:21:47 -0800

    Revert "Remove unnecessary files"
    This reverts commit 19fdd6270a82ad2dd7125ffad9d5f6361d8f265d.

---
+++ frontend/src/main.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect} from 'react';
+import axios from 'axios'
+import { useLocation } from 'react-router-dom';
+import './App.css';
+
+function CogniFlow() {
+  // const [userInput, setUserInput] = useState("")
+  const [summary, setSummary] = useState("")
+  const [status, setStatus] = useState("")
+  let [listItems, setListItems] = useState([])
+
+  const { state } = useLocation()
+  const id = state.id
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+    let response = await axios.post('http://127.0.0.1:5000/post_human_message', formData)
+    let convo = JSON.parse(response.data.conversation)
+    let listItems = convo.map((conversationElement) =>
+    <div>{conversationElement}</div>
+    );
+    setListItems(listItems)
+    console.log(listItems)
+    response = await axios.post('http://127.0.0.1:5000/get_ai_response', formData)
+
+    // Kill these two afterwards; just for debugging
+    setSummary(response.data.summary)
+    setStatus(response.data.status)
+    console.log(response.data.conversation)
+    console.log(status)
+    convo = JSON.parse(response.data.conversation)
+    listItems = convo.map((conversationElement) =>
+    <div>{conversationElement}</div>
+    );
+    setListItems(listItems)
+  }
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="flex-container">
+            {listItems}
+        </div>
+        {/* <p>DEBUGGING: ID={id}</p> */}
+        <form onSubmit={handleSubmit}>
+          <input
+            name="ID"
+            value={id}
+            type="hidden"
+          />
+          <label>
+            <br/>
+            <textarea
+              name="HUMAN_MESSAGE"
+              defaultValue=""
+            //   value={humanText}
+            //   onChange={e => setHumanText(e.target.value)}
+              rows={5}
+              cols={80}
+            />
+          </label>
+          <br/>
+          <button type="submit">Return</button>
+        </form>
+      </header>
+    </div>
+  )
+}
+
+export default CogniFlow;
